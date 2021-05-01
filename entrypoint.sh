03bcec2aab7d2c98ee30fe91651790b7d4f28137
Yuki Kirii <yukirii@microsoft.com>
2021-05-01 21:37:06 +0900

    Copy required files if working dir is not /blog

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,11 @@
 
 umask 0000
 
+if [ "$PWD" != "/blog" ]; then
+  [ ! -e "$PWD/gulpfile.js" ] && cp /blog/gulpfile.js $PWD
+  [ ! -e "$PWD/package.json" ] && cp /blog/package.json $PWD
+  [ ! -e "$PWD/package-lock.json" ] && cp /blog/package-lock.json $PWD
+  [ ! -e "$PWD/node_modules" ] && cp -r /blog/node_modules $PWD
+fi
+
 exec "$@"
